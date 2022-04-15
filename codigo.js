49232a84bc176d62951736ea1fd134a85be4a66d
jeronimo <jsequeiros@itba.edu.ar>
2022-04-15 20:22:09 -0300

    avances

---
+++ codigo.js
@@ -0,0 +1,9 @@
+// problemas logicos o matematicos
+// la programacion es una herrmaienta para resolver problemas
+// pensamiento computacional
+// descomposicion: descomponer el problema
+// patron: encontrar similitudes
+// abstraccion: achicar el problema, eliminar los superfluo
+// algoritmo: pasos para resolver el problema
+
+console.log("hola mundo")
