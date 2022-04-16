90f2a0046306a9c099a2c69c0fde23f8e2a510ed
jeronimo <jsequeiros@itba.edu.ar>
2022-04-16 17:10:53 -0300

    avances

--- codigo.js
+++ codigo.js
@@ -6,4 +6,31 @@
 // abstraccion: achicar el problema, eliminar los superfluo
 // algoritmo: pasos para resolver el problema
 
+/*
+  Variables? espacio en memoria donde se almacena un valor. 
+  Que es? Un cajon
+  Como armo una variable? 
+  let nombreDeLaVariable ="valor"
+  ej: let nombre="jeronimo"
+  = es el operador de asignación 
+
+  Tipos de datos: 
+numerico: enteros o con coma (integer, float)
+string: cadena de caracteres
+
++ > suma o concatenacion (juntar strings)
+Nan = not a number 
+
+Javascript usa camelCase
+las constantes se escriben en MAYUSCULA
+
+/ funciona para la division y % para saber el resto
+
+= asigna un valor a una variable 
+== pregunta si el valor coincide
+=== pregunta si el valor y el tipo de dato coinciden 
+
+
+  */
+
 console.log("hola mundo")
