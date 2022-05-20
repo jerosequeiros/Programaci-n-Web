0391439fd54a9bfe161d3bb18eb2c0e0021b6396
jeronimo <jsequeiros@itba.edu.ar>
2022-05-20 16:51:52 -0300

    avances

---
+++ Raiz Nomada/main.js
@@ -0,0 +1,6 @@
+console.log("soy externo")
+
+function HacerAparecer(){
+    let contenedor = document.getElementById("productos")
+    contenedor.innerHTML = 
+ "Canasto de palma" + " Canasto de cuero" + " Jarra enlozada"}
