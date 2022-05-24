4a789d36cd511381b6722192532d09b3f985f5e3
jeronimo <jsequeiros@itba.edu.ar>
2022-05-24 20:30:54 -0300

    AVANCES

--- Raiz Nomada/main.js
+++ Raiz Nomada/main.js
@@ -1,6 +1,21 @@
 console.log("soy externo")
 
-function HacerAparecer(){
-    let contenedor = document.getElementById("productos")
+function HacerAparecer1(){
+    let contenedor = document.getElementById("canastos")
     contenedor.innerHTML = 
- "Canasto de palma" + " Canasto de cuero" + " Jarra enlozada"}
+ "Canasto de palma <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas"}
+
+ function HacerAparecer2(){
+    let contenedor = document.getElementById("enlozados")
+    contenedor.innerHTML = 
+ "enlozados horno barro steve <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas"}
+
+ function HacerAparecer3(){
+    let contenedor = document.getElementById("materas")
+    contenedor.innerHTML = 
+ "descripcion de las materas <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas"}
+
+ function HacerAparecer4(){
+    let contenedor = document.getElementById("mesa")
+    contenedor.innerHTML = 
+ "descripcion de la mesa entera <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas"}
