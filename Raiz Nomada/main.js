d7e2442f6503ac7d9526dfb7b57ef7d5bc7b9d0f
jeronimo <jsequeiros@itba.edu.ar>
2022-06-02 15:42:39 -0300

    avances

--- Raiz Nomada/main.js
+++ Raiz Nomada/main.js
@@ -1,22 +1,26 @@
-console.log("soy externo")
+console.log("soy externo");
 
-function HacerAparecer1(){
-    let contenedor = document.getElementById("canastos")
-    contenedor.innerHTML = 
-  "Canasto de cuero crudo trenzado <br> Medidas: diametro 40cm, Alto 28cm <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas"}
+function HacerAparecer1() {
+  let contenedor = document.getElementById("canastos");
+  contenedor.innerHTML =
+    "Canasto de cuero crudo trenzado <br> Medidas: diametro 40cm, Alto 28cm <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas";
+}
 
- function HacerAparecer2(){
-    let contenedor = document.getElementById("enlozados")
-    contenedor.innerHTML = 
- "enlozados <br> Jarra gallo colorado bombé <br> Platos 24cm de diamettro y 3cm de profundidad <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas"}
- 
+function HacerAparecer2() {
+  let contenedor = document.getElementById("enlozados");
+  contenedor.innerHTML =
+    "enlozados <br> Jarra gallo colorado bombé <br> Platos 24cm de diamettro y 3cm de profundidad <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas";
+}
 
- function HacerAparecer3(){
-    let contenedor = document.getElementById("materas")
-    contenedor.innerHTML = 
- "Materas de cuero peludas <br> Tamaño ideal <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas"}
+function HacerAparecer3() {
+  let contenedor = document.getElementById("materas");
+  contenedor.innerHTML =
+    "Materas de cuero peludas <br> Tamaño ideal <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas";
+}
 
- function HacerAparecer4(){
-    let contenedor = document.getElementById("mesa")
-    contenedor.innerHTML = 
- "descripcion de la mesa entera <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas"}
+function HacerAparecer4() {
+  let contenedor = document.getElementById("mesa");
+  contenedor.innerHTML =
+    "Descripción de la mesa entera <br> Materiales nobles que aportan calidez a vos y tus espacios <br> Artesanias Correntinas";
+}
+
